6455e0278f3f43a492af90f3e6374681a0b03fb4
grajo <adolfo.rodriguez.guillen@gmail.com>
2020-06-20 15:56:59 +0100

    adding calculartor logic

--- Web/src/app/Components/Calculator/calculator/calculator.component.ts
+++ Web/src/app/Components/Calculator/calculator/calculator.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  public amount: number;
+  public tax: number;
+  public result: number;
+  public showResult: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.amount = 0;
+    this.tax = 0;
+    this.result = 0;
+    this.showResult = false;
+  }
+
+  calculate() {
+    this.result = this.amount + this.tax;
+    this.showResult = true;
+    //this.result to call the backend passing the specific data
+  }
+
+  reset() {
+    this.ngOnInit();
   }
 
 }
